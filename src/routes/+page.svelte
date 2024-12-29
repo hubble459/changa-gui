41ef55f7dfe7777e55b8dc9e08b9894b610dfc09
hubble459 <geraldd459@gmail.com>
2024-12-29 16:51:34 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { Changa } from 'changa';
+
+    const changa = new Changa();
+</script>
+
+<h1>changa</h1>
+<hr>
+<div>
+    <table>
+        <tbody>
+            <tr>
+                <th>hostname</th>
+                <th>working</th>
+            </tr>
+        {#each changa.hostnames as hostname}
+            <tr>
+                <td>{hostname}</td>
+                <td>{changa.accepts() ? 'yes' : 'no'}</td>
+            </tr>
+        {/each}
+        </tbody>
+    </table>
+</div>
+
+<style>
+    th, td {
+        padding: 0em 1em;
+    }
+</style>
